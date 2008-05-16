d75fb31307f04fc4f5be0165416cb7c98aec6af3
Jeffrey Chu <jochu@uci.edu>
2008-05-16 08:58:48 -0700

    Add autoload info to simplify configuring clojure mode

--- clojure-paredit.el
+++ clojure-paredit.el
@@ -1,7 +1,13 @@
+(require 'clojure-auto)
 
-(defun clojure-paredit-hook ()
-  (paredit-mode +1)
-  (define-key clojure-mode-map "{" 'paredit-open-brace)
-  (define-key clojure-mode-map "}" 'paredit-close-brace))
+(autoload 'paredit-mode "paredit" "Parenthesis editing minor mode" t)
 
-(add-hook 'clojure-mode-hook 'clojure-paredit-hook)
+(eval-after-load "clojure-mode"
+  '(progn
+     (defun clojure-paredit-hook () (paredit-mode +1))
+     (add-hook 'clojure-mode-hook 'clojure-paredit-hook)
+
+     (define-key clojure-mode-map "{" 'paredit-open-brace)
+     (define-key clojure-mode-map "}" 'paredit-close-brace)))
+
+(provide 'clojure-paredit)
