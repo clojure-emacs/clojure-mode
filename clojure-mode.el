ed0bb136efe40cd5fbbd76b2063e12aa2f03b567
Phil Hagelberg <technomancy@gmail.com>
2010-05-13 22:28:40 -0700

    Release clojure-mode 1.7.1.

--- clojure-mode.el
+++ clojure-mode.el
@@ -6,7 +6,7 @@
 ;;          Lennart Staflin <lenst@lysator.liu.se>
 ;;          Phil Hagelberg <technomancy@gmail.com>
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/ClojureMode
-;; Version: 1.7
+;; Version: 1.7.1
 ;; Keywords: languages, lisp
 
 ;; This file is not part of GNU Emacs.
