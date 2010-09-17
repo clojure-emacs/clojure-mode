f8bf03f9450f2e8837980d531bc7820c4e925dce
Phil Hagelberg <technomancy@gmail.com>
2010-09-16 21:45:17 -0700

    Release 1.8.0.

--- clojure-mode.el
+++ clojure-mode.el
@@ -6,7 +6,7 @@
 ;;          Lennart Staflin <lenst@lysator.liu.se>
 ;;          Phil Hagelberg <technomancy@gmail.com>
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/ClojureMode
-;; Version: 1.7.1
+;; Version: 1.8.0
 ;; Keywords: languages, lisp
 
 ;; This file is not part of GNU Emacs.
@@ -16,25 +16,21 @@
 ;; Provides font-lock, indentation, and navigation for the Clojure
 ;; language. (http://clojure.org)
 
-;;; Installation:
-
-;; If you use ELPA (http://tromey.com/elpa), you can install via the
-;; M-x package-list-packages interface. This is preferrable as you
-;; will have access to updates automatically.
-
-;; If you need to install by hand for some reason:
-
-;; (0) Add this file to your load-path, usually the ~/.emacs.d directory.
-;; (1) Either:
-;;     Add this to your .emacs config: (require 'clojure-mode)
-;;     Or generate autoloads with the `update-directory-autoloads' function.
-
 ;; See also the swank-clojure package for better interaction with
-;; Clojure subprocesses. Note that M-x clojure-install functionality
-;; has been moved to that package and is deprecated here.
+;; Clojure subprocesses.
 
 ;; Users of older Emacs (pre-22) should get version 1.4:
 ;; http://github.com/technomancy/clojure-mode/tree/1.4
+
+;; Install using package.el. You will need to add repo.technomancy.us
+;; to your archive list:
+
+;; (add-to-list 'package-archives "http://repo.technomancy.us/emacs")
+
+;; If you use a version of Emacs prior to 24 that doesn't include
+;; package.el, you can get it from http://bit.ly/pkg-el. If you have
+;; an older package.el installed from tromey.com, you should upgrade
+;; in order to support installation from multiple sources.
 
 ;; Paredit users:
 
