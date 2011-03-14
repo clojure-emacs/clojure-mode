4abcd792a5c03766e64b734439f065da4e087f40
Phil Hagelberg <technomancy@gmail.com>
2011-03-14 18:36:45 +0000

    Compose fixtures in clojure-test-run-test.

--- clojure-test-mode.el
+++ clojure-test-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Phil Hagelberg <technomancy@gmail.com>
 ;; URL: http://emacswiki.org/cgi-bin/wiki/ClojureTestMode
-;; Version: 1.5.2
+;; Version: 1.5.3
 ;; Keywords: languages, lisp, test
 ;; Package-Requires: ((slime "20091016") (clojure-mode "1.7"))
 
@@ -164,7 +164,29 @@
                                                    ((file-position 3) 1)
                                                    (:line event)))])))
      (binding [*test-out* *out*]
-       (old-report event)))")))
+       (old-report event)))
+
+   (defn clojure-test-mode-test-one-var [test-ns test-name]
+     (let [v (ns-resolve test-ns test-name)
+           once-fixture-fn (join-fixtures (::once-fixtures (meta (find-ns test-ns))))
+           each-fixture-fn (join-fixtures (::each-fixtures (meta (find-ns test-ns))))]
+       (once-fixture-fn
+        (fn []
+          (when (:test (meta v))
+            (each-fixture-fn (fn [] (test-var v))))))))
+
+    ;; adapted from test-ns
+    (defn clojure-test-mode-test-one-in-ns [ns test-name]
+      (binding [*report-counters* (ref *initial-report-counters*)]
+        (let [ns-obj (the-ns ns)]
+          (do-report {:type :begin-test-ns, :ns ns-obj})
+          ;; If the namespace has a test-ns-hook function, call that:
+          (if-let [v (find-var (symbol (str (ns-name ns-obj)) \"test-ns-hook\"))]
+            ((var-get v))
+            ;; Otherwise, just test every var in the namespace.
+            (clojure-test-mode-test-one-var ns test-name))
+          (do-report {:type :end-test-ns, :ns ns-obj}))
+        (do-report (assoc @*report-counters* :type :summary)))) ")))
 
 (defun clojure-test-get-results (result)
   (clojure-test-eval
@@ -266,7 +288,7 @@ Retuns the problem overlay if such a position is found, otherwise nil."
                          "(clojure.test/run-tests)")
        #'clojure-test-get-results))))
 
-;; TODO: compose clojure.test fixtures and use them here
+;; TODO: run tests in region
 (defun clojure-test-run-test ()
   "Run the test at point."
   (interactive)
@@ -278,8 +300,11 @@ Retuns the problem overlay if such a position is found, otherwise nil."
        (slime-eval-async
         `(swank:interactive-eval
           ,(format "(do (load-file \"%s\")
-                      (when (:test (meta (var %s))) (%s) (cons (:name (meta (var %s))) (:status (meta (var %s))))))"
-                   (buffer-file-name) test-name test-name test-name test-name))
+                        (clojure-test-mode-test-one-in-ns '%s '%s)
+                        (cons (:name (meta (var %s))) (:status (meta (var %s)))))"
+                   (buffer-file-name)
+                   (slime-current-package) test-name
+                   test-name test-name))
         (lambda (result-str)
           (let ((result (read result-str)))
             (if (cdr result)
