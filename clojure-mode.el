8cf92d7d1ae5d40e8caaaf0bffa1c2db01ac96b9
Phil Hagelberg <technomancy@gmail.com>
2011-09-20 11:19:19 -0500

    Version 1.11.1.

--- clojure-mode.el
+++ clojure-mode.el
@@ -6,7 +6,7 @@
 ;;          Lennart Staflin <lenst@lysator.liu.se>
 ;;          Phil Hagelberg <technomancy@gmail.com>
 ;; URL: http://github.com/technomancy/clojure-mode
-;; Version: 1.11.0
+;; Version: 1.11.1
 ;; Keywords: languages, lisp
 
 ;; This file is not part of GNU Emacs.
