22654d8d29b8c835d39d0c9316560bd409b354d8
Bozhidar Batsov <bozhidar@batsov.com>
2013-08-06 18:10:25 +0300

    Update the links in the front matter

--- clojure-mode.el
+++ clojure-mode.el
@@ -5,7 +5,7 @@
 ;; Authors: Jeffrey Chu <jochu0@gmail.com>
 ;;          Lennart Staflin <lenst@lysator.liu.se>
 ;;          Phil Hagelberg <technomancy@gmail.com>
-;; URL: http://github.com/technomancy/clojure-mode
+;; URL: http://github.com/clojure-emacs/clojure-mode
 ;; Version: 2.1.0
 ;; Keywords: languages, lisp
 
@@ -14,20 +14,20 @@
 ;;; Commentary:
 
 ;; Provides font-lock, indentation, and navigation for the Clojure
-;; language. (http://clojure.org)
+;; programming language (http://clojure.org).
 
 ;; Users of older Emacs (pre-22) should get version 1.4:
-;; http://github.com/technomancy/clojure-mode/tree/1.4
+;; http://github.com/clojure-emacs/clojure-mode/tree/1.4
 
 ;; Slime integration has been removed; see the 1.x releases if you need it.
 
-;; Using clojure-mode with paredit is highly recommended. Use paredit
+;; Using clojure-mode with paredit is highly recommended.  Use paredit
 ;; as you would with any other minor mode; for instance:
 ;;
 ;;   ;; require or autoload paredit-mode
 ;;   (add-hook 'clojure-mode-hook 'paredit-mode)
 
-;; See nREPL.el (http://github.com/kingtim/nrepl.el) for
+;; See nREPL.el (http://github.com/clojure-emacs/nrepl.el) for
 ;; better interaction with subprocesses via nREPL.
 
 ;;; License:
