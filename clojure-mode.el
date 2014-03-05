53b6df281406b0b3814159589858e0b3f26ed248
Hoàng Minh Thắng <p@banphim.net>
2014-03-06 04:37:34 +0700

    fix indent for fresh, run and run* in core.logic

--- clojure-mode.el
+++ clojure-mode.el
@@ -968,6 +968,11 @@ use (put-clojure-indent 'some-symbol 'defun)."
   (are 1)
   (use-fixtures 'defun)
 
+  ;; core.logic
+  (run 'defun)
+  (run* 'defun)
+  (fresh 'defun)
+  
   ;; core.async
   (alt! 0)
   (alt!! 0)
