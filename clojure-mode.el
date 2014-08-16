a70cebb82fd2a7edefd2254e1added833491af1d
Bozhidar Batsov <bozhidar@tradeo.com>
2014-08-16 08:47:23 +0300

    Remove some obsolete data from the commentary section

--- clojure-mode.el
+++ clojure-mode.el
@@ -19,11 +19,6 @@
 ;; Provides font-lock, indentation, and navigation for the Clojure
 ;; programming language (http://clojure.org).
 
-;; Users of older Emacs (pre-22) should get version 1.4:
-;; http://github.com/clojure-emacs/clojure-mode/tree/1.4
-
-;; Slime integration has been removed; see the 1.x releases if you need it.
-
 ;; Using clojure-mode with paredit is highly recommended.  Use paredit
 ;; as you would with any other minor mode; for instance:
 ;;
