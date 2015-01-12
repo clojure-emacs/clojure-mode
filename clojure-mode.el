8ae3c3938c2c72458470e64ca413e24f2b149ac5
Oleh Krehel <ohwoeowho@gmail.com>
2015-01-12 20:19:56 +0100

    [Fix #269] Check for bobp in `clojure-in-docstring-p'
    * clojure-mode.el (clojure-in-docstring-p): Don't call
    `(get-text-property 0)`, since it will throw.

--- clojure-mode.el
+++ clojure-mode.el
@@ -280,8 +280,9 @@ ENDP and DELIMITER."
 
 (defsubst clojure-in-docstring-p ()
   "Check whether point is in a docstring."
-  (eq (get-text-property (1- (point-at-eol)) 'face)
-      'font-lock-doc-face))
+  (unless (bobp)
+    (eq (get-text-property (1- (point-at-eol)) 'face)
+        'font-lock-doc-face)))
 
 (defsubst clojure-docstring-fill-prefix ()
   "The prefix string used by `clojure-fill-paragraph'.
