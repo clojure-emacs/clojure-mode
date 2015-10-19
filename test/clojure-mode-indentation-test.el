53f9ebdefbb796da51b92ba1edb423a6a2508904
Artur Malabarba <bruce.connor.am@gmail.com>
2015-10-19 14:29:13 +0100

    Add regression tests for function specs

--- test/clojure-mode-indentation-test.el
+++ test/clojure-mode-indentation-test.el
@@ -298,6 +298,44 @@ values of customisable variables."
   "(msg' 1
       10)")
 
+
+(defun indent-cond (indent-point state)
+  (goto-char (elt state 1))
+  (let ((pos -1)
+        (base-col (current-column)))
+    (forward-char 1)
+    ;; `forward-sexp' will error if indent-point is after
+    ;; the last sexp in the current sexp.
+    (condition-case nil
+        (while (and (<= (point) indent-point)
+                    (not (eobp)))
+          (clojure-forward-logical-sexp 1)
+          (cl-incf pos))
+      ;; If indent-point is _after_ the last sexp in the
+      ;; current sexp, we detect that by catching the
+      ;; `scan-error'. In that case, we should return the
+      ;; indentation as if there were an extra sexp at point.
+      (scan-error (cl-incf pos)))
+    (+ base-col (if (evenp pos) 0 2))))
+(put-clojure-indent 'test-cond #'indent-cond)
+
+(defun indent-cond-0 (_indent-point _state) 0)
+(put-clojure-indent 'test-cond-0 #'indent-cond-0)
+
+(def-full-indent-test function-spec
+  "(when me
+  (test-cond
+    x
+  1
+    2
+  3))"
+  "(when me
+  (test-cond-0
+x
+1
+2
+3))")
+
 
 ;;; Misc
 
