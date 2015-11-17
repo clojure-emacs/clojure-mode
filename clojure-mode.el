cb3c275d3fa692351c9bfef38281660caa2aecc6
Artur Malabarba <bruce.connor.am@gmail.com>
2015-11-17 12:06:51 +0000

    Fix non-optional optional arguments in forward-logical-sexp

--- clojure-mode.el
+++ clojure-mode.el
@@ -1191,6 +1191,7 @@ Sexps that don't represent code are ^metadata or #reader.macros."
 This will skip over sexps that don't represent objects, so that ^hints and
 #reader.macros are considered part of the following sexp."
   (interactive "p")
+  (unless n (setq n 1))
   (if (< n 0)
       (clojure-backward-logical-sexp (- n))
     (let ((forward-sexp-function nil))
@@ -1206,6 +1207,7 @@ This will skip over sexps that don't represent objects, so that ^hints and
 This will skip over sexps that don't represent objects, so that ^hints and
 #reader.macros are considered part of the following sexp."
   (interactive "p")
+  (unless n (setq n 1))
   (if (< n 0)
       (clojure-forward-logical-sexp (- n))
     (let ((forward-sexp-function nil))
