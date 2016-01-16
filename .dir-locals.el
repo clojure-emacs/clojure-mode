19c54ba4764f41acb87b1508178c0811ed143f79
Artur Malabarba <bruce.connor.am@gmail.com>
2016-01-16 07:31:55 +0000

    Add a .dir-locals.el file akin to CIDER's

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+
+((nil
+  (bug-reference-url-format . "https://github.com/clojure-emacs/cider/issues/%s")
+  (indent-tabs-mode)
+  (fill-column . 80)
+  (checkdoc-arguments-in-order-flag))
+ (emacs-lisp-mode
+  (bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")))
