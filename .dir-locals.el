e85f13069b38b73f4eaf85d0d274d0068505b144
Bozhidar Batsov <bozhidar@batsov.com>
2016-01-16 13:16:29 +0200

    Make it clearer that we're disabling indent-tabs-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((nil
   (bug-reference-url-format . "https://github.com/clojure-emacs/clojure-mode/issues/%s")
-  (indent-tabs-mode)
+  (indent-tabs-mode . nil)
   (fill-column . 80)
   (checkdoc-arguments-in-order-flag))
  (emacs-lisp-mode
