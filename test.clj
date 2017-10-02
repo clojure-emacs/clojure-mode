46a9942cce46cc3715a239b7b31834391293fb88
Rostislav Svoboda <thebost@gmail.com>
2017-10-02 16:03:28 +0200

    Namespace definition examples

--- test.clj
+++ test.clj
@@ -1,7 +1,21 @@
 ;;; font locking
 (ns clojure-mode.demo
-  (:require [clojure.something]
-            [something.s]))
+  (:require
+   [clojure.something]
+   [something.s]
+   [oneword]
+   ;; TODO mixedCase shouldn't be font-locked to clojure-interop-method-face
+   [mixedCase]
+   [CamelCase]))
+
+;; examples of valid namespace definitions
+(comment
+  (ns .validns)
+  (ns =validns)
+  (ns .ValidNs=<>?+|?*.)
+  (ns ValidNs<>?+|?*.b*ar.ba*z)
+  (ns other.valid.ns)
+  (ns foo.bar))
 
 (comment ;; for indentation
   (with-hi heya
