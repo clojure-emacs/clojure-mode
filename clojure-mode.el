8073725fdc5ef56a6b248175376e0c07c077ce54
Bozhidar Batsov <bozhidar@batsov.com>
2019-09-13 11:11:24 +0300

    [Fix #542] Fix a typo

--- clojure-mode.el
+++ clojure-mode.el
@@ -1090,7 +1090,7 @@ point) to check."
 (put 'defmacro 'clojure-doc-string-elt 2)
 (put 'definline 'clojure-doc-string-elt 2)
 (put 'defprotocol 'clojure-doc-string-elt 2)
-(put 'deftask 'clojure-doc-string-eld 2) ;; common Boot macro
+(put 'deftask 'clojure-doc-string-elt 2) ;; common Boot macro
 
 ;;; Vertical alignment
 (defcustom clojure-align-forms-automatically nil
