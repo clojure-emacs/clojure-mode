7757fac80cf4a8f6421e33de7877449ea621ef5f
Bozhidar Batsov <bozhidar@batsov.com>
2020-09-22 09:56:59 +0300

    Extend the commentary for clojure-mode-extra-font-locking

--- clojure-mode-extra-font-locking.el
+++ clojure-mode-extra-font-locking.el
@@ -12,7 +12,12 @@
 
 ;;; Commentary:
 
-;; Provides additional font-locking for clojure-mode.
+;; Provides additional font-locking for clojure-mode.  This font-locking
+;; used to be part of clojure-mode up to version 3.0, but it was removed
+;; due to its unreliable nature (the implementation is quite primitive
+;; and font-locks symbols without any regard for what they resolve to).
+;; CIDER provides much more reliable font-locking, that's based on the runtime
+;; state of your Clojure application.
 
 ;;; License:
 
