b53c714931821f41e04554e5f4cb863f9db9e7e8
p4v4n <pavan.mantha99@gmail.com>
2023-03-07 00:52:52 +0530

    Add another test for clojure-sort-ns

--- test/clojure-mode-util-test.el
+++ test/clojure-mode-util-test.el
@@ -138,6 +138,18 @@
     (:require [my-app.views [user-page :as user-page]]
               [rum.core :as rum] ;comment\n))")))
 
+  (it "should sort requires in a basic ns with comments in the end"
+    (with-clojure-buffer "(ns my-app.core
+    (:require [rum.core :as rum] ;comment
+              [my-app.views [user-page :as user-page]]
+              ;;[comment2]\n))"
+      (clojure-sort-ns)
+      (expect (buffer-string) :to-equal
+              "(ns my-app.core
+    (:require [my-app.views [user-page :as user-page]]
+              [rum.core :as rum] ;comment\n
+              ;;[comment2]\n))")))
+
   (it "should also sort imports in a ns"
     (with-clojure-buffer "\n(ns my-app.core
     (:require [my-app.views [front-page :as front-page]]
